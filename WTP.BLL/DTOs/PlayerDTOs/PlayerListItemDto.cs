25e810d0351e5a7d3a4844c372d8dfec583b1a44
Vardan <asatrian.v@gmail.com>
2019-05-26 13:46:48 +0300

    Merge with development

--- WTP.BLL/DTOs/PlayerDTOs/PlayerListItemDto.cs
+++ WTP.BLL/DTOs/PlayerDTOs/PlayerListItemDto.cs
@@ -9,17 +9,12 @@ namespace WTP.BLL.DTOs.PlayerDTOs
     {
         //[JsonProperty("id")]
         public int Id { get; set; }
-        //[JsonProperty("n")]
         public string Name { get; set; }
-        //[JsonProperty("g")]
         public string Game { get; set; }
-        //[JsonProperty("r")]
         public string Rank { get; set; }
-        //[JsonProperty("s")]
         public string Server { get; set; }
-        //[JsonProperty("go")]
         public string Goal { get; set; }
-        //[JsonProperty("a")]
         public string About { get; set; }
+        public int Decency { get; set; }
     }
 }
