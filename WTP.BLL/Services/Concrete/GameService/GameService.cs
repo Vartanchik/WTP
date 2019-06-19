694bb542788f94b99200a259dbb20b044ea1ffea
Dmytro Minochkin <dmytro.minochkin@gmail.com>
2019-06-19 21:37:21 +0300

    fix

--- WTP.BLL/Services/Concrete/GameService/GameService.cs
+++ WTP.BLL/Services/Concrete/GameService/GameService.cs
@@ -30,7 +30,6 @@ namespace WTP.BLL.Services.Concrete.GameService
         public async Task CreateOrUpdateAsync(GameDto dto, int? adminId = null)
         {
             var game = _mapper.Map<Game>(dto);
-            game.Id = 0;
 
             await _uow.Games.CreateOrUpdate(game);
             await _uow.CommitAsync();
